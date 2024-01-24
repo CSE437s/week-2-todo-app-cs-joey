3a5f8f652f31927e1e52d009dbf05204705c52ab
7byte9 <121839119+7byte9@users.noreply.github.com>
2024-01-23 19:16:55 -0600

    update

--- todos2.js
+++ todos2.js
@@ -1,16 +1,26 @@
+const firebaseConfig = {
+};
+firebase.initializeApp(firebaseConfig);
+
 let todoID = 0;
 let textContent = "sample content";
 let addForm = document.getElementById("addForm");
 let inContent = document.getElementById('inID');
 
-function newTodo(){
+function newTodo() {
     let newDiv = document.createElement('div');
-    newDiv.id = todoID; 
+    newDiv.id = todoID;
     textContent = inContent.innerText;
     todoID++;
     newDiv.className = 'item';
     newDiv.innerHTML = textContent;
     document.body.appendChild(newDiv);
+
+    const firebaseRef = firebase.database().ref('todos/' + todoID);
+    firebaseRef.set({ textContent });
 }
 
+
+
 addForm.addEventListener("click", newTodo);
+
